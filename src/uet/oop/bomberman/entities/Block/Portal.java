594a4c9e802909cab681d853004da87bcedd4c93
Nguyen Dang Quang <quangnguyenvk@gmail.com>
2022-09-29 22:06:30 +0700

    Update Portal.java

--- src/uet/oop/bomberman/entities/Block/Portal.java
+++ src/uet/oop/bomberman/entities/Block/Portal.java
@@ -6,6 +6,7 @@ import uet.oop.bomberman.entities.Entity;
 public class Portal extends Entity {
 
     public static boolean isPortal = false;         // Variables use to display the portal img when player pass the level
+
     public Portal(int x, int y, Image img) {
         super(x, y, img);
     }
