29818ae12a9540598fb7def721059ee21e1ac37a
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2022-10-20 12:09:13 +0700

    update something

--- src/uet/oop/bomberman/entities/Items/WallPassItem.java
+++ src/uet/oop/bomberman/entities/Items/WallPassItem.java
@@ -9,6 +9,9 @@ import static uet.oop.bomberman.BombermanGame.*;
 import static uet.oop.bomberman.BombermanGame.player;
 
 public class WallPassItem extends Items {
+
+    public static int speed = 1;
+
     public WallPassItem(int x, int y, Image img) {
         super(x, y, img);
     }
@@ -23,9 +26,9 @@ public class WallPassItem extends Items {
 
     @Override
     public void update() {
-        for (Entity entity: stillObjects) {
-            if (entity instanceof  WallPassItem && (!isReceived)) {
-                if (listIsKilled[entity.getX() / 32][entity.getY()/32] == 4) {
+        for (Entity entity : stillObjects) {
+            if (entity instanceof WallPassItem && (!isReceived)) {
+                if (listIsKilled[entity.getX() / 32][entity.getY() / 32] == 4) {
                     entity.setLife(false);
                     entity.setImg(Sprite.powerup_wallpass.getFxImage());
                 }
@@ -34,12 +37,13 @@ public class WallPassItem extends Items {
 
         if (!isReceived) {
             if (player.getX() == this.x && player.getY() == this.y) {
-                if(!this.getLife()) {
-                        this.setImg(Sprite.grass.getFxImage());
-                        this.isReceived = true;
-                        player.setCanPass(true);
+                if (!this.getLife()) {
+                    this.setImg(Sprite.grass.getFxImage());
+                    this.isReceived = true;
+                    player.setCanPass(true);
                 }
             }
         }
     }
 }
+
