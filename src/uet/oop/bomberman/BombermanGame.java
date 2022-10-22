443a4f805ea084a15ef4db5502d813c9f0d12a72
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2022-10-22 09:39:38 +0700

    update highscore

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -25,9 +25,9 @@ import uet.oop.bomberman.entities.Items.SpeedItem;
 import uet.oop.bomberman.entities.Monster.*;
 import uet.oop.bomberman.graphics.Sprite;
 import uet.oop.bomberman.Control.Move;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+
+import java.io.*;
+
 import uet.oop.bomberman.entities.Block.Bomb;
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +44,16 @@ import static uet.oop.bomberman.entities.Block.Portal.*;
 public class BombermanGame extends Application {
     public static int score = 0;
     public static int highScore;
+
+    Scanner input;
+
+    {
+        try {
+            input = new Scanner(new File("res/score/highscore.txt"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static boolean isOver = false;
 
@@ -100,6 +110,7 @@ public class BombermanGame extends Application {
 
     @Override
     public void start(Stage stage) {
+        highScore =input.nextInt();
         // Tao Canvas
         canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
         gc = canvas.getGraphicsContext2D();
@@ -202,90 +213,6 @@ public class BombermanGame extends Application {
             }
         };
         timer.start();
-
-//        createMap();
-//        player = new Bomber(1, 1, Sprite.player_right.getFxImage());
-//        entities.add(player);
-//        enemies.add(new Minvo(7, 3, Sprite.minvo_left1.getFxImage()));
-//        enemies.add(new Oneal(9, 3, Sprite.oneal_left1.getFxImage()));
-//        enemies.add(new  Doll(21, 6, Sprite.doll_left1.getFxImage()));
-//        enemies.add(new Kondoria(3, 1, Sprite.kondoria_left1.getFxImage()));
-//        enemies.add(new Ballom(7, 1, Sprite.balloom_left1.getFxImage()));
-//        stillObjects.add(new Portal(23, 13, Sprite.transparent.getFxImage()));
-//    }
-//
-//    public void createMap() {
-//////        for (int i = 0; i < WIDTH; i++) {
-//////            for (int j = 0; j < HEIGHT; j++) {
-//////                Entity object;
-//////                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
-//////                    object = new Wall(i, j, Sprite.wall.getFxImage());
-//////                }
-//////                else {
-//////                    object = new Grass(i, j, Sprite.grass.getFxImage());
-//////                }
-//////                stillObjects.add(object);
-//////            }
-//////        }
-//
-//        String pathLevel = "res/levels/Level1.txt";
-//        File fileName = new File(pathLevel);        //TODO: Create object fileName contain info of the level.
-//        try (FileReader reader = new FileReader(fileName)) {            // Try - catch to create map.
-//            Scanner sc = new Scanner(reader);           // Create obj sc from class Scanner.
-//            String mapInfo = sc.nextLine();                // Get input from line 1 in string type.
-//
-//            StringTokenizer token = new StringTokenizer(mapInfo);      // Read 1 string in line.
-//            // parseInt() method: convert String to Integer
-//            _gameLevel = Integer.parseInt(token.nextToken());
-//            _mapHeight = Integer.parseInt(token.nextToken());
-//            _mapWidth = Integer.parseInt(token.nextToken());
-//
-//            while (sc.hasNextLine()) {              // If sc can read more line.
-//                objIdx = new int[_mapWidth][_mapHeight];            // Create new obj mapIdx in main file.
-//                listIsKilled = new int[_mapWidth][_mapHeight];         // Create new obj listKill in main file.
-//                for (int i = 0; i < _mapHeight; ++i) {
-//                    String lineInfo = sc.nextLine();                // Get input from line.
-//                    StringTokenizer tokenLineInfo = new StringTokenizer(lineInfo);      // Read info from lineInfo.
-//
-//                    for (int j = 0; j < _mapWidth; ++j) {
-//                        int value = Integer.parseInt(tokenLineInfo.nextToken());
-//                        Entity object;                              // Create obj named object from class Entity.
-//
-//                        // This if - else statement running, and we got a full map for a game.
-//                        // Through the program, in the for-loop statement, we can get the map according to each loop it passed.
-//                        switch (value) {
-//                            case 1:
-//                                object = new Portal(j, i, Sprite.portal.getFxImage());
-//                                value = 0;
-//                                break;
-//                            case 2:
-//                                object = new Wall(j, i, Sprite.wall.getFxImage());
-//                                break;
-//                            case 3:
-//                                object = new Brick(j, i, Sprite.brick.getFxImage());
-//                                break;
-//                            case 6:
-//                                object = new SpeedItem(j, i, Sprite.brick.getFxImage());
-//                                break;
-//                            case 7:
-//                                object = new FlameItem(j, i, Sprite.brick.getFxImage());
-//                                break;
-//                            default:
-//                                object = new Grass(j, i, Sprite.grass.getFxImage());
-//                        }
-//
-//                        objIdx[j][i] = value;
-//                        stillObjects.add(object);
-//
-//                    }
-//                }
-//
-//            }
-//
-//        } catch (IOException e) {               // Catch exception.
-//            e.printStackTrace();                // printStackTrace(): Help to understand where the problem is actually happening.
-//        }
-
     }
     public void update() {
         for (int i = 0; i < entities.size(); i++) {
@@ -329,6 +256,10 @@ public class BombermanGame extends Application {
     }
 
     public void updateMenu() {
+        if(score>highScore) {
+            highScore=score;
+            
+        }
             level.setText("Level: " + _gameLevel);
         scoreText.setText("Score: " + score);
         highscore.setText("Highscore: " + highScore);
